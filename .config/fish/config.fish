8d39ee53692657aacfc5fe044d2dcbddab54cf71
yk1979 <yukiirisawa1979@gmail.com>
2023-08-17 15:18:53 +0900

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,15 +1,10 @@
-eval (nodenv init)
-eval (rbenv init)
-pyenv init - | source
-set -U fish_user_paths /usr/local/bin
-set -U fish_user_paths $HOME/.nodenv/bin
+set -U fish_user_paths /opt/homebrew/bin/
+set -x VOLTA_HOME $HOME/.volta
+set -x PATH $VOLTA_HOME/bin $PATH
 set -x GOPATH $HOME/go
 set -x PATH $PATH $GOPATH/bin
 set -x JAVA_HOME /Library/Java/JavaVirtualMachines/amazon-corretto-11.jdk/Contents/Home
 set -x PATH $JAVA_HOME/bin $PATH
-status --is-interactive; and source (nodenv init -|psub)
-status --is-interactive; and source (rbenv init -|psub)
-status is-interactive; and pyenv init --path | source
 alias nr="npm run"
 alias nrd="npm run dev"
 alias nrb="npm run build"
@@ -23,15 +18,3 @@ alias gcob="git checkout -b"
 alias gc="git commit"
 alias gcm="git commit -m"
 alias gpuo="git branch --show-current | xargs git push -u origin"
-alias incone="scutil --nc start インコネ_v02"
-alias uncone="scutil --nc stop インコネ_v02"
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/uu168581/google-cloud-sdk/path.fish.inc' ]; . '/Users/uu168581/google-cloud-sdk/path.fish.inc'; end
-
-# pnpm
-set -gx PNPM_HOME "/Users/uu168581/Library/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
